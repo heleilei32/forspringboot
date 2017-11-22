7c929267bc4443d37253de30455ddb6edd746b59
lenovo <28218734@qq.com>
2017-11-22 13:40:16 +0800

    Initial commit

---
+++ src/main/java/com/lei/forspringboot/ForspringbootApplication.java
@@ -0,0 +1,12 @@
+package com.lei.forspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForspringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForspringbootApplication.class, args);
+	}
+}
